b0b6416b0192599bda41e4debc8e046c6fb99f77
S.Gromov <zz-gromov@ya.ru>
2026-08-26 11:49:11 +0300

    chore: удалить OLD-src

--- OLD-src/references/apps/react-vite/examples/domains/authentication/source-errors/is-account-locked-source-error.ts
+++
@@ -1,8 +0,0 @@
-import { isIdentityApiError } from '@/infra/identity-api'
-
-/**
- * Распознаёт wire-code заблокированной учётной записи.
- */
-export const isAccountLockedSourceError = (error: unknown): boolean => {
-  return isIdentityApiError(error, 423, 'account_locked')
-}
